8ff17f7d1c55ca7f12203e022daf673dfd67f84d
Caridad Rivera <ccrive10@gmail.com>
2019-02-21 14:05:38 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,21 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count
   attr_reader :title
 
   def initialize(title)
     @title = title
   end
 
+GENRES =[]
+
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
 
+#create the setter, get the variables and then push the variable value into the array 
+
+def genre=(genre)
+  @genre = genre
+  GENRES << genre
+  end
 end
